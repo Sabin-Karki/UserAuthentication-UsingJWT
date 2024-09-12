0a43afefbedba47f9b6a3b4edd4b96473738ba5f
Sabin-Karki <168570435+Sabin-Karki@users.noreply.github.com>
2024-09-12 19:23:32 +0545

    Add files via upload

---
+++ UserDTO.java
@@ -0,0 +1,14 @@
+package com.example.finance.DTO;
+
+import lombok.Data;
+
+@Data
+    public class UserDTO {
+
+        private String username;
+        private String email;
+        private String password;
+    }
+
+
+
